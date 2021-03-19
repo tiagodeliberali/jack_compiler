a15c081babab906f65982c5b1e2a65c57fc70147
tiagodeliberali <tiago.santos@quintoandar.com.br>
2021-03-19 10:54:13 -0300

    added builder tests

--- src/builder.rs
+++ src/builder.rs
@@ -21,3 +21,30 @@ fn clean_line(line: &str) -> String {
     let line = line[0];
     String::from(line.trim())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn clean_line_with_spaces() {
+        let token = clean_line("   test(x);    ");
+
+        assert_eq!("test(x);", token);
+    }
+
+    #[test]
+    fn clean_line_with_simple_comment() {
+        let token = clean_line("   test(x);    // should test with coment");
+
+        assert_eq!("test(x);", token);
+    }
+
+    #[test]
+    fn clean_line_with_special_comment() {
+        let token = clean_line("   test(x);    /** should test with coment */");
+
+        assert_eq!("test(x);", token);
+    }
+}
